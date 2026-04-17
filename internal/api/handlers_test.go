157fb873974c03f7059571c387848967eff93f60
meshenka <meshee.knight@gmail.com>
2026-04-17 22:46:54 +0200

    test(backend): better assertions of responses
    TODO: how to make sure we received the right response according to
    locale?

--- internal/api/handlers_test.go
+++ internal/api/handlers_test.go
@@ -39,12 +39,14 @@ func TestGetDescriptorsHandler(t *testing.T) {
 		mux.ServeHTTP(w, req)
 
 		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
 
 		var resp api.DescriptorResponse
 		err := json.Unmarshal(w.Body.Bytes(), &resp)
 		assert.NoError(t, err)
-		// We expect french words like "Stoïque" or "Attrayant".
-		// Since we're picking randomly, let's just make sure it doesn't fail.
 		assert.Len(t, resp.Descriptors, 3)
+		for _, desc := range resp.Descriptors {
+			assert.NotEmpty(t, desc)
+		}
 	})
 }
